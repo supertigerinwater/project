55648c493a959a6ae519b14c48bc00cce5bc4aeb
Fangquan Wang <fqwang@Fangquans-MacBook-Pro.local>
2016-11-19 23:46:56 -0500

    add the basic map to our index page

---
+++ angular-2-template-master/app/ts/map.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-map',
+    templateUrl: 'app/ts/map.component.html' 
+})
+
+export class MapComponent {
+	
+}
---
+++ angular-2-template-master/app/ts/map.ts
@@ -0,0 +1,9 @@
+export class Map {
+	startPosition: string;
+	endPosition: string;
+
+	constructor(startPosition: string, endPosition: string) {
+		this.startPosition = startPosition;
+		this.endPosition = endPosition;
+	}
+}
